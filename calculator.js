69f0cb1d9795db866619166b6f00355832967b02
Intiyaj Darji <154606172+Imtiyaj-ahmad@users.noreply.github.com>
2024-05-10 04:18:57 +0545

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,19 @@
-
+function Solve(val) {
+   var v = document.getElementById('res');
+   //storing data of result in variable v
+   v.value += val;
+   //sum up store value and val
+}
+function Result() {
+   var num1 = document.getElementById('res').value;
+   var num2 = eval(num1);
+   document.getElementById('res').value = num2;
+}
+function Clear() {
+   var inp = document.getElementById('res');
+   inp.value = '';
+}
+function Back() {
+   var ev = document.getElementById('res');
+   ev.value = ev.value.slice(0,-1);
+}
